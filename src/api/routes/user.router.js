f62c401bf874e8209bc13565f4ec5a03c4f640a7
nemoToLara <stiven19r.h@gmail.com>
2023-06-30 11:47:38 -0500

    solution to err #01 user-router

--- src/api/routes/user.router.js
+++ src/api/routes/user.router.js
@@ -1,7 +1,7 @@
 import { Router } from 'express';
 
 import UserController from '../controllers/user.controller.js';
-// melara
+// me
 const User = new UserController();
 
 const router = Router();
