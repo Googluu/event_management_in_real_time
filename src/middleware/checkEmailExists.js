70f7b70151e478dad73d2c5b92287d3789d46600
Sneyder <sneyder.h.rodriguez@gmail.com>
2023-07-18 11:03:38 -0500

    middleware of validation

---
+++ src/middleware/checkEmailExists.js
@@ -0,0 +1,14 @@
+import { User } from '../database/entities/user.entity.js';
+
+export const checkEmailExists = async (req, res) => {
+  try {
+    const user = await User.findOne({ email: req.body.email });
+    if (user) {
+      return res.status(400).json({ error: 'El email ya está en uso' });
+    }
+    next();
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: 'Hubo un error en el servidor' });
+  }
+};
--- src/middleware/err.handler.js
+++ src/middleware/err.handler.js
@@ -1,7 +1,16 @@
+import { isBoom } from '@hapi/boom';
+
 export const errorHanlder = (err, _, res, next) => {
   res.status(500).json({
     message: err.message,
     stack: err.stack,
   });
   next(err);
+};
+
+export const boomErrHandler = (err, req, res, next) => {
+  if (isBoom(err)) {
+    const { payload, statusCode } = err.output;
+    res.status(statusCode).json(payload);
+  } else next(err);
 };
